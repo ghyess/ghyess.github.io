6af7cb50049e36ba893989a7ae221037f27926bb
ghyess <39326466+ghyess@users.noreply.github.com>
2018-10-16 11:50:42 +0900

    main-js update
    first 18-10-16 am11:50

--- res3/main.js
+++ res3/main.js
@@ -5,4 +5,34 @@ $(document).ready(function() {
     $("#gnb").mouseleave(function() {
         $("#gnb").stop().slideUp(300);
     });
+    
+    //슬라이드
+    var wd = parseInt($("#main ").width());
+    var cnt = $("#main .mainBox li").length;
+    var intv = setInterval(function() { nAni();},4000);
+    
+    function nAni() {
+        $("#main .mainBox").not(":animated").animate({"margin-left":-wd+"px"},1000, function(){
+            $("#main .mainBox li").eq(0).appendTo($("#main .mainBox"));
+            $("#main .mainBox").css("margin-left", "0px");
+        });
+    }
+    function pAni() {
+        $("#main .mainBox li").eq(cnt-1).prependTo($("#main .mainBox"));
+        $("#main .mainBox").css("margin-left", -wd+"px");
+        $("#main .mainBox").not(":animated").animate({"margin-left":"0px"}, 1000);
+    };
+    
+    //버튼박스 좌
+    $("#main #left").click(function() {
+        clearInterval(intv);
+        pAni();
+        intv = setInterval(function() { nAni(); }, 4000);
+    });
+    //버튼박스 우
+    $("#main #right").click(function() {
+        clearInterval(intv);
+        nAni();
+        intv = setInterval(function() { nAni(); }, 4000);
+    });
 });

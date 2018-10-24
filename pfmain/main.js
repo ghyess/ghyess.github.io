dd2e3be3785982a4c888177a51082ca0e1bb4d77
ghyess <39326466+ghyess@users.noreply.github.com>
2018-10-24 16:07:07 +0900

    main js update
    20181024

--- pfmain/main.js
+++ pfmain/main.js
@@ -1,17 +1,26 @@
+
 $(document).ready(function(){
-    var speed = 800; //스피드 수치로 사용할 변수
+   var ht = parseInt($(window).height());
     
-    function scrolling(obj){
-        if(!obj){ //예외처리
-            $('html, body').animate({scrollTop:0}, speed);
+    $(window).on("mousewheel", function (e) {
+        if (e.originalEvent.wheelDelta < 0) {
+            $("html,body").not(":animated").animate({
+                scrollTop: "+=" + ht + "px"
+            }, 700);
         } else {
-            var posTop = $(obj).offset().top; // posTop = 매개변수로 들어온 컨텐츠 obj의 offset().top - 네비높이
-            $('html, body').animate({scrollTop:posTop}, speed) // body의 스크롤 이동 : posTop
+            $("html, body").not(":animated").animate({
+                scrollTop: "-=" + ht + "px"
+            }, 700);
         }
-    };
-    $("#gnb .menu li a").click(function(){
-        var direction = $(this).attr("href"); // direction = 클릭한 요소의 href 속성으로이동
-        scrolling( direction ); // direction을 인자로 함수 실행
         return false;
+    }); 
+    
+    $("#gnb a").click(function(){
+       var pgscroll = $($(this).attr("href")).offset().top;
+        
+        $("html,body").not(":animated").animate({
+           scrollTop:pgscroll 
+        }, 700);
     });
+    
 });

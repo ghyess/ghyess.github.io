268ab6d4a1bb1402858f04a9d0e86f610cc81c63
ghyess <39326466+ghyess@users.noreply.github.com>
2018-10-30 12:47:06 +0900

    sub page-img update
    2018.10.30

--- pfmain/main.js
+++ pfmain/main.js
@@ -1,7 +1,6 @@
-
 $(document).ready(function(){
+    //페이지 스크롤링
    var ht = parseInt($(window).height());
-    
     $(window).on("mousewheel", function (e) {
         if (e.originalEvent.wheelDelta < 0) {
             $("html,body").not(":animated").animate({
@@ -23,4 +22,23 @@ $(document).ready(function(){
         }, 700);
     });
     
+     // 이미지 마우스 오버시
+    $("#pro2 .img-inner").on("mouseover", function(){
+        $(this).stop().animate({"width":"24%"},900);
+        //$(this).find('img').stop().animate({"width":"105%"},800);
+    });
+    // 마우스 아웃시
+    $("#pro2 .img-inner").on("mouseout", function(){
+        $(this).stop().animate({"width":"270px"},900);
+    });
+    
+    // 이미지 마우스 오버시
+    $("#pro3 .img-inner").on("mouseover", function(){
+        $(this).stop().animate({"width":"35%"},900);
+        //$(this).find('img').stop().animate({"width":"105%"},800);
+    });
+    // 마우스 아웃시
+    $("#pro3 .img-inner").on("mouseout", function(){
+        $(this).stop().animate({"width":"360px"},900);
+    });
 });
